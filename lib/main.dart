982e41098e431242143453fe6880d7d99877fc68
BhavHacker <109847685+BhavHacker@users.noreply.github.com>
2023-11-13 17:30:12 +0530

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:catelog_flutter/pages/home_page.dart';
+import 'package:catelog_flutter/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: '/home',
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
